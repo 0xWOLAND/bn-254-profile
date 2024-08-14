a4407d2441c048d4dcaa6971ce06bd4b0ac5b847
Bhargav Annem <bhargav.annem@gmail.com>
2024-08-14 16:55:04 -0700

    feat: clean repo

--- script/src/bin/prove.rs
+++ script/src/bin/prove.rs
@@ -14,3 +14,16 @@ fn main() {
     // Verify proof.
     client.verify(&proof, &vk).expect("verification failed");
 }
+
+// use sp1_sdk::utils;
+// use sp1_sdk::{ProverClient, SP1Stdin};
+// pub const ELF: &[u8] = include_bytes!("../../../program/elf/riscv32im-succinct-zkvm-elf");
+
+// fn main() {
+//     utils::setup_logger();
+
+//     let mut stdin = SP1Stdin::new();
+
+//     let client = ProverClient::new();
+//     let (mut public_values, _) = client.execute(ELF, stdin).run().expect("failed to prove");
+// }
